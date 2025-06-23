9eff36fb133b80e30d64dbc46c65e82f44c1438a
Michael Gernoth <michael@gernoth.net>
2025-06-23 22:04:19 +0200

    Minor bug fix for potential buffer underrun in heathmsc.cpp
    Taken from upstream http://www.ke5fx.com/heather/readme.htm

--- heathmsc.cpp
+++ heathmsc.cpp
@@ -9170,8 +9170,10 @@ u08 day_of_week(int d, int m, int y)      /* 0=Sunday  1=Monday ... */
 {
 static u08 dow_info[] = {0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4};
 
+   int n = m - 1;
+   if (n < 0) n += 12;
    if(m < 3) y -= 1;
-   return (y + y/4 - y/100 + y/400 + dow_info[m-1] + d) % 7;
+   return (y + y/4 - y/100 + y/400 + dow_info[n] + d) % 7;
 }
 
 int nth_dow_in(int nth, int dow, int month, int year)
